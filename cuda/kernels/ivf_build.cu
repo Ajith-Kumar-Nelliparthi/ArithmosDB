52172d9a62296e7402eeb75fa3282d2b2d53cbb2
Ajith Kumar <nelliparthi123@gmail.com>
2026-03-12 13:39:03 +0530

    ivf build kernel updated

--- cuda/kernels/ivf_build.cu
+++ cuda/kernels/ivf_build.cu
@@ -17,12 +17,11 @@
  * It also keeps track of the original indices of the vectors for later retrieval during search.
  */
 
- __global__ void reorder_kernel(const float* __restrict__ d_vectors, // original n x d matrix of vectors
-                                const int* d_assignments, // cluster id for each vector
-                                const int* d_offsets, // starting position of each cluster
-                                int* d_working_offsets, // current offset for each cluster (updated atomically
-                                float* d_inverted_vectors, // out: reordered n x d matrix of vectors
-                                int* d_inverted_ids, // out: original indices of vectors in the new order
+ __global__ void reorder_kernel(const float* __restrict__ d_vectors,
+                               const int* __restrict__ d_assignments,
+                               float* __restrict__ d_inverted_vectors,
+                               int* __restrict__ d_inverted_ids,
+                               int* __restrict__ d_working_offsets, // out: original indices of vectors in the new order
                                 int n, int d) {
                                     
     int vec_idx = blockIdx.x * blockDim.x + threadIdx.x;
@@ -72,81 +71,68 @@ extern "C" void build_ivf(const float* d_vectors,
     int *d_assignments = nullptr;
     CUDA_CHECK(cudaMalloc(&d_assignments, n_vectors * sizeof(int)));
 
-    // run k-means to find centeroids and assignments
+    // 1. RUN K-MEANS TO FIND CENTROIDS AND ASSIGNMENTS
     run_kmeans(d_vectors, index->d_centeroids, d_assignments, n_vectors, dim, nlist, max_iter);
-}
 
-/**
- * Coarse Search KERNEL
- * This kernel calculates the distance from one query to all k centeroids.
- */
+    // 2. REORDER VECTORS INTO INVERTED LISTS
+    int* d_working_offsets = nullptr;
+    CUDA_CHECK(cudaMalloc(&d_working_offsets, nlist * sizeof(int)));
+    CUDA_CHECK(cudaMemset(d_working_offsets, 0, nlist * sizeof(int)));
 
- __global__ void coarse_search_kernel(const float* d_query, // query vector (size d)
-                                        const float* d_centeroids, // all k centeroids (size k x d)
-                                        float* d_coarse_distances, // out: distances from the query to each centeroid (size k)
-                                        int k, // no.of centeroids
-                                        int d) {
-    int centeroid_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int query_idx = blockIdx.y;
+    {
+        dim3 block(THREADS_PER_BLOCK);
+        dim3 grid((n_vectors + block.x - 1) / block.x);
 
-    if (centeroid_idx < k) {
-        // store the query in shared memory for faster access
-        extern __shared__ float s_query[];
-        for (int i=threadIdx.x; i<d; i+=blockDim.x) {
-            s_query[i] = d_query[query_idx * d + i];
-        }
-        __syncthreads();
+        reorder_kernel<<<grid, block>>>(d_vectors, d_assignments,
+                                        index->d_inverted_vectors,
+                                        index->d_inverted_ids,
+                                        d_working_offsets,
+                                        n_vectors, dim);
 
-        // compute the distance from the query to the centeroid
-        float sum = 0.0f;
-        for (int i=0; i<d; i++) {
-            float diff = s_query[i] - d_centeroids[centeroid_idx * d + i];
-            sum += diff * diff;
-        }
-        d_coarse_distances[query_idx * k + centeroid_idx] = sum;
+        CUDA_CHECK(cudaGetLastError());
+        CUDA_CHECK(cudaDeviceSynchronize());
     }
-}
 
-/**
- * FINE SEARCH KERNEL
- * This kernel computes the distances from one query to all vectors 
- * and store in the "shortlisted cluster".
- */
+    // 3. Build list_sizes and list_offsets using thrust
+    thrust::device_ptr<int> assign_ptr(d_assignments);
+    thrust::device_vector<int> keys(n_vectors);
+    thrust::copy(assign_ptr, assign_ptr + n_vectors, keys.begin());
 
-__global__ void fine_search_kernel(const float* d_query,
-                                    const float* d_inverted_vectors,
-                                    const int* d_inverted_ids,
-                                    const int* d_list_offsets,
-                                    const int* d_list_sizes,
-                                    const int* d_selected_cluster_ids,
-                                    float* d_neighbor_distances,
-                                    int* d_neighbor_ids,
-                                    int nprobe,
-                                    int d) {
-    int probe_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (probe_idx < nprobe) {
-        int cluster_id = d_selected_cluster_ids[probe_idx];
-        int start_offset = d_list_offsets[cluster_id];
-        int list_size = d_list_sizes[cluster_id];
+    // sort assignments to group by cluster
+    thrust::sort(keys.begin(), keys.end());
 
-        // store the query in shared memory for faster access
-        extern __shared__ float s_query[];
-        int query_idx = blockIdx.y;
-        for (int i= threadIdx.x; i<d; i+=blockDim.x) {
-            s_query[i] = d_query[query_idx * d + i];
-        }
-        __syncthreads();
+    // count how many vectors in each cluster
+    thrust::device_vector<int> unique_clusters(nlist);
+    thrust::device_vector<int> list_sizes(nlist);
 
-        // compute distances from the query to all vectors in this cluster
-        for (int i=0; i<list_size; i++) {
-            int vec_idx = d_inverted_ids[start_offset + i];
-            float sum = 0.0f;
-            for (int j=0; j<d; j++) {
-                float diff = s_query[j] - d_inverted_vectors[(start_offset + i) * d + j];
-                sum += diff * diff;
-            }
-            d_neighbor_distances[probe_idx * list_size + i] = sum;
-            d_neighbor_ids[probe_idx * list_size + i] = vec_idx;
-        }
-    }
+    auto new_end = thrust::reduce_by_key(keys.begin(), keys.end(),
+                                    thrust::constant_iterator<int>(1),
+                                    unique_clusters.begin(),
+                                    list_sizes.begin());
+    
+    int num_nonempty = new_end.first - unique_clusters.begin();
+
+    // fill list_sizes
+    thrust::device_ptr<int> sizes_ptr(index->d_list_sizes);
+    thrust::fill(sizes_ptr, sizes_ptr + nlist, 0);
+
+    thrust::scatter(list_sizes.begin(), list_sizes.begin() + num_nonempty,
+                    unique_clusters.begin(),
+                    sizes_ptr);
+
+    // Build prefix sums → list_offsets
+    thrust::device_ptr<int> offsets_ptr(index->d_list_offsets);
+    thrust::exclusive_scan(sizes_ptr, sizes_ptr + nlist,
+                           offsets_ptr);
+    // Last element should be total n_vectors
+    CUDA_CHECK(cudaMemcpy((offsets_ptr + nlist).get(),
+                          &n_vectors,
+                          sizeof(int),
+                          cudaMemcpyHostToDevice));
+
+    // Cleanup
+    CUDA_CHECK(cudaFree(d_assignments));
+    CUDA_CHECK(cudaFree(d_working_offsets));
+
+    printf("IVF index built: %d vectors, dim=%d, nlist=%d\n", n_vectors, dim, nlist);
 }
